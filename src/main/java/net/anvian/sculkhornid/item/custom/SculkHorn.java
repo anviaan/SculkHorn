b026e02437a33d4e2f51faed507801a25d2f96eb
anviaan <andre.carbajal.vargas@gmail.com>
2022-06-10 19:18:55 -0500

    progress

--- src/main/java/net/anvian/sculkhornid/item/custom/SculkHorn.java
+++ src/main/java/net/anvian/sculkhornid/item/custom/SculkHorn.java
@@ -6,7 +6,9 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 public class SculkHorn extends Item{
     public SculkHorn(Settings settings) {
@@ -15,16 +17,15 @@ public class SculkHorn extends Item{
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+
         if (world.isClient) {
             if (user.isCreative()) {
-                world.playSound(user, user.getX(), user.getY(), user.getZ(),
-                        SoundEvents.ENTITY_WARDEN_SONIC_BOOM, SoundCategory.AMBIENT, 3.0f, 1.0f);
+                playsound(world, user);
                 user.getItemCooldownManager().set(this, 600); //add a cooldown 30s
-
             } else {
                 if (user.experienceLevel >= 10) {
-                    world.playSound(user, user.getX(), user.getY(), user.getZ(),
-                            SoundEvents.ENTITY_WARDEN_SONIC_BOOM, SoundCategory.AMBIENT, 3.0f, 1.0f);
+                    playsound(world, user);
                     user.experienceLevel -= 10;
                     user.getItemCooldownManager().set(this, 600); //add a cooldown 30s
                 }
@@ -32,4 +33,16 @@ public class SculkHorn extends Item{
         }
         return super.use(world, user, hand);
     }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.TOOT_HORN;
+    }
+
+    private static void playsound(World world, PlayerEntity user){
+        world.playSoundFromEntity(user, user, SoundEvents.ENTITY_WARDEN_SONIC_BOOM, SoundCategory.RECORDS, 10.0f, 1.0f);
+        world.emitGameEvent(GameEvent.INSTRUMENT_PLAY, user.getPos(), GameEvent.Emitter.of(user));
+    }
+
+
 }
