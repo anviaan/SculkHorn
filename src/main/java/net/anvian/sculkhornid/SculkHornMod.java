318e0bf6d34b719edffc5cfa29a9258493ebb73c
anvian <andre.carbajal.vargas@gmail.com>
2022-07-16 07:29:32 -0500

    add a logger

--- src/main/java/net/anvian/sculkhornid/SculkHornMod.java
+++ src/main/java/net/anvian/sculkhornid/SculkHornMod.java
@@ -3,9 +3,12 @@ package net.anvian.sculkhornid;
 import net.anvian.sculkhornid.config.ModConfigs;
 import net.anvian.sculkhornid.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SculkHornMod implements ModInitializer {
 	public static final String MOD_ID = "sculkhornid";
+	public static final Logger LOGGER = LoggerFactory.getLogger("sculkhornid");
 
 	@Override
 	public void onInitialize() {
@@ -13,5 +16,7 @@ public class SculkHornMod implements ModInitializer {
 		ModConfigs.registerConfigs();
 
 		ModItems.registerModItems();
+
+		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/anvian/sculkhornid/mixin/SculkHornMixin.java
+++ src/main/java/net/anvian/sculkhornid/mixin/SculkHornMixin.java
@@ -1,9 +1,16 @@
 package net.anvian.sculkhornid.mixin;
 
+import net.anvian.sculkhornid.SculkHornMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class SculkHornMixin {
-
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        SculkHornMod.LOGGER.info("This line is printed by an example mod mixin!");
+    }
 }
