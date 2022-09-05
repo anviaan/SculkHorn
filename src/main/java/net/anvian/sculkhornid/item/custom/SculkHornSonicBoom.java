de44657e634e099d3d738e12627859a74597714d
anvian <andre.carbajal.vargas@gmail.com>
2022-09-05 14:38:50 -0500

    it works! :D, needs changes

--- src/main/java/net/anvian/sculkhornid/item/custom/SculkHornSonicBoom.java
+++ src/main/java/net/anvian/sculkhornid/item/custom/SculkHornSonicBoom.java
@@ -1,7 +1,12 @@
 package net.anvian.sculkhornid.item.custom;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.WolfEntity;
@@ -12,9 +17,9 @@ import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
@@ -25,27 +30,67 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SculkHornSonicBoom extends Item {
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+
     public SculkHornSonicBoom(Settings settings) {
         super(settings);
+
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", 6.0f, EntityAttributeModifier.Operation.ADDITION));
+        this.attributeModifiers = builder.build();
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        if(slot == EquipmentSlot.MAINHAND) {
+            return attributeModifiers;
+        }
+        return super.getAttributeModifiers(slot);
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        if(!world.isClient){
+        user.setCurrentHand(hand);
+        return super.use(world, user, hand);
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.SPYGLASS;
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 20;
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        super.usageTick(world, user, stack, remainingUseTicks);
+
+        if(getMaxUseTime(stack) - remainingUseTicks == 1) {
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_WARDEN_SONIC_CHARGE, SoundCategory.PLAYERS, 3.0f, 1.0f);
+        }
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if(!world.isClient) {
             spawnSonicBoom(world, user);
-            if(!user.isCreative()){
-                user.getItemCooldownManager().set(this, 300);
-                itemStack.damage(1, user, (entity) -> entity.sendToolBreakStatus(hand));
+            if(user instanceof PlayerEntity player) {
+                //player.getItemCooldownManager().set(this, 20 * 5);
+                stack.damage(1, user, x -> x.sendToolBreakStatus(Hand.MAIN_HAND));
             }
         }
 
-        return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
+        return super.finishUsing(stack, world, user);
     }
 
-    private void spawnSonicBoom(World world, LivingEntity user){
+    // https://github.com/Draylar/identity/blob/arch-1.19/common/src/main/java/draylar/identity/mixin/PlayerSonicBoomMixin.java
+    private void spawnSonicBoom(World world, LivingEntity user) {
         world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_WARDEN_SONIC_BOOM, SoundCategory.PLAYERS, 3.0f, 1.0f);
 
+        // Raycast out for sonic boom effect
         float heightOffset = 1.6f;
         int distance = 16;
         Vec3d target = user.getPos().add(user.getRotationVector().multiply(distance));
@@ -64,12 +109,12 @@ public class SculkHornSonicBoom extends Item {
         }
 
         // Don't hit ourselves
-        hit.remove((PlayerEntity) (Object) this);
+        hit.remove(user);
 
         // Find
         for (Entity hitTarget : hit) {
-            if (hitTarget instanceof LivingEntity living) {
-                living.damage(DamageSource.sonicBoom((PlayerEntity) (Object) this), 10.0f);
+            if(hitTarget instanceof LivingEntity living) {
+                living.damage(DamageSource.sonicBoom(user), 10.0f);
                 double vertical = 0.5 * (1.0 - living.getAttributeValue(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE));
                 double horizontal = 2.5 * (1.0 - living.getAttributeValue(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE));
                 living.addVelocity(normalized.getX() * horizontal, normalized.getY() * vertical, normalized.getZ() * horizontal);
