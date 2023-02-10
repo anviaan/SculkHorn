68d4530036340d3d949b6df08d420b3d18f19512
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-02-10 12:12:28 -0500

    the order of the configuration has been changed

--- src/main/java/net/anvian/sculkhornid/config/ModConfigs.java
+++ src/main/java/net/anvian/sculkhornid/config/ModConfigs.java
@@ -8,18 +8,18 @@ import io.wispforest.owo.config.annotation.Sync;
 @Sync(Option.SyncMode.OVERRIDE_CLIENT)
 public class ModConfigs{
     //sculk horn area
+    public double AREA_DAMAGE = 12;
     public double AREA_RADIUS = 3.5;
     public int AREA_COOLDOWN = 300;
-    public double AREA_DAMAGE = 12;
     public int AREA_EXPERIENCE_LEVEL = 5;
     public int AREA_REMOVE_EXPERIENCE = -55;
     public int AREA_DURABILITY = 350;
 
     //sculk horn distance
     public double DISTANCE_DAMAGE = 8;
+    public int DISTANCE_DISTANCE = 16;
     public int DISTANCE_COOLDOWN = 200;
     public int DISTANCE_EXPERIENCE_LEVEL = 5;
     public int DISTANCE_REMOVE_EXPERIENCE = -55;
-    public int DISTANCE_DISTANCE = 16;
     public int DISTANCE_DURABILITY = 500;
 }
