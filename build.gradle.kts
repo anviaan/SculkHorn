745770ac62b7f8bcf12a76e86f5db391239537c3
anviaan <andre.carbajal.vargas@gmail.com>
2025-04-18 07:46:19 -0500

    Update game version format in publish YAML files for consistency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,11 @@ plugins {
 
 val MOD_VERSION = project.property("version") as String
 val ARCHIVE_NAME = project.property("mod_name") as String
-val COMPATIBLE_VERSIONS = listOf("1.21", "1.21.1")
 
 tasks.register("printEnv") {
     doLast {
         val envFile = File(System.getenv("GITHUB_ENV"))
         envFile.appendText("MOD_VERSION=$MOD_VERSION\n")
         envFile.appendText("RELEASE_NAME=$ARCHIVE_NAME-$MOD_VERSION\n")
-        envFile.appendText("GAME_VERSIONS=${COMPATIBLE_VERSIONS.joinToString(",")}\n")
     }
 }
