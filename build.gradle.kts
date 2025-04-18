261cfe7913f7c2d644b5eb49ec6f57f941253c6a
anviaan <andre.carbajal.vargas@gmail.com>
2025-04-18 07:32:33 -0500

    Refactor compatible versions format in build.gradle.kts for improved readability

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 
 val MOD_VERSION = project.property("version") as String
 val ARCHIVE_NAME = project.property("mod_name") as String
-val COMPATIBLE_VERSIONS = "[1.21, 1.21.1]"
+val COMPATIBLE_VERSIONS = listOf("1.21", "1.21.1")
 
 tasks.register("printEnv") {
     doLast {
         val envFile = File(System.getenv("GITHUB_ENV"))
         envFile.appendText("MOD_VERSION=$MOD_VERSION\n")
         envFile.appendText("RELEASE_NAME=$ARCHIVE_NAME-$MOD_VERSION\n")
-        envFile.appendText("GAME_VERSIONS=$COMPATIBLE_VERSIONS\n")
+        envFile.appendText("GAME_VERSIONS=${COMPATIBLE_VERSIONS.joinToString(",")}\n")
     }
 }
