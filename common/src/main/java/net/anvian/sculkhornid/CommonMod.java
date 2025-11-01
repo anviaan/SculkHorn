6f8cf6aaa892af5c377056b5535c86ea7e2bd7d1
anviaan <andre.carbajal.vargas@gmail.com>
2025-10-31 23:16:42 -0500

    Refactor configuration handling and remove dependencies on ForgeConfigApiPort

--- common/src/main/java/net/anvian/sculkhornid/CommonMod.java
+++ common/src/main/java/net/anvian/sculkhornid/CommonMod.java
@@ -1,25 +1,17 @@
 package net.anvian.sculkhornid;
 
-import net.anvian.anvianslib.config.TelemetryConfigManager;
+import net.anvian.anvianslib.platform.Services;
 import net.anvian.anvianslib.util.LibUtil;
-import net.anvian.sculkhornid.platform.Services;
-import net.minecraft.SharedConstants;
+import net.anvian.sculkhornid.core.config.ModConfigs;
 
 public class CommonMod {
+    public static ModConfigs configs = new ModConfigs(ModConfigs.SculkHornConfig.class, Constants.LOG);
+
     public static void init() {
         Constants.LOG.info("Hello from " + Constants.MOD_ID);
 
-        LibUtil.generateConfigPath(Constants.MOD_ID, Services.PLATFORM.getGameConfigDirectory());
+        configs.initialize(Services.PLATFORM.getConfigPath().resolve(Constants.MOD_ID).toFile(), Constants.MOD_ID);
 
-        TelemetryConfigManager.initialize(Services.PLATFORM.getGameConfigDirectory().resolve(Constants.MOD_ID).toFile());
-        if (TelemetryConfigManager.getConfig().enableTelemetry) {
-            TelemetryConfigManager.sendTelemetryData(
-                    Constants.MOD_ID,
-                    "3.0",
-                    SharedConstants.getCurrentVersion().getName(),
-                    Services.PLATFORM.getPlatformName(),
-                    !Services.PLATFORM.isDevelopmentEnvironment()
-            );
-        }
+        LibUtil.setupTelemetry(Constants.MOD_ID, "3.1");
     }
 }
--- forge/src/main/java/net/anvian/sculkhornid/ForgeMod.java
+++ forge/src/main/java/net/anvian/sculkhornid/ForgeMod.java
@@ -1,15 +1,11 @@
 package net.anvian.sculkhornid;
 
 import net.anvian.sculkhornid.core.ModTab;
-import net.anvian.sculkhornid.core.config.ModConfigs;
 import net.anvian.sculkhornid.core.registry.ModItemRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
 
 @Mod(Constants.MOD_ID)
 public class ForgeMod {
@@ -18,10 +14,6 @@ public class ForgeMod {
         CommonMod.init();
 
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        Constants.LOG.info("Registering config for " + Constants.MOD_NAME + "...");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ModConfigs.SPEC, Constants.MOD_ID + "-config.toml");
-        ModConfigs.loadConfig(ModConfigs.SPEC, FMLPaths.CONFIGDIR.get().resolve(Constants.MOD_ID).resolve(Constants.MOD_ID + "-config.toml"));
 
         Constants.LOG.info("Registering items for " + Constants.MOD_NAME + "...");
         ModItemRegistry.ITEMS.register(bus);
